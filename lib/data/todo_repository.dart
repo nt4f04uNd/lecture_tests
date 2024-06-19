b34c30f62c4270a0ea8d25aa2cc4d72077491a45
dushesatwork <guid.empty@gmail.com>
2024-06-19 13:43:40 +0300

    todo repository

--- lib/data/todo_repository.dart
+++ lib/data/todo_repository.dart
@@ -4,10 +4,10 @@ import 'package:lecture_about_tests/domain/todo.dart';
 class TodoRepository {
   /// InMemory хранилище данных
   Set<Todo> _todos = {
-    const Todo(id: 1, title: 'Task 1', isCompleted: false),
     const Todo(id: 2, title: 'Task 2', isCompleted: false),
-    const Todo(id: 3, title: 'Task 3', isCompleted: false),
     const Todo(id: 4, title: 'Task 4', isCompleted: false),
+    const Todo(id: 3, title: 'Task 3', isCompleted: false),
+    const Todo(id: 1, title: 'Task 1', isCompleted: false),
     const Todo(id: 5, title: 'Task 5', isCompleted: false),
   };
 
